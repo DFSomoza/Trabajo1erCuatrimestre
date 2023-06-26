c3d06242820d717d66068ca5c5c9de7439164215
Daniel <dfsistemas64@hotmail.com>
2023-06-26 16:39:27 -0300

    Agregado de index.js y Ventanas emergentes

---
+++ index.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById('detalleModal1');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('detalleModal2');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('detalleModal3');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
